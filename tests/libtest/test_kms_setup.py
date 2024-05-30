0f185865ae8f20b3c8568d36fbedde24ca064c1a
fbalak <fbalak@redhat.com>
2024-05-30 15:51:35 +0200

    add kms setup
    Signed-off-by: fbalak <fbalak@redhat.com>

---
+++ tests/libtest/test_kms_setup.py
@@ -0,0 +1,17 @@
+import logging
+import time
+
+from ocs_ci.framework.testlib import libtest
+from ocs_ci.utility import kms as KMS
+
+logger = logging.getLogger(__name__)
+
+
+@libtest
+def setup_kms():
+    """
+    Test of RHV get_vm_status() method implementation
+    VM  of healthy OCS Cluster has 'up' status by default.
+    """
+    kms = KMS.get_kms_deployment()
+    kms.deploy()
